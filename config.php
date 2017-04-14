372383c5490dcd42872d0b8b9c14536edf21e301
Quang <quangprobt@gmail.com>
2017-04-15 02:15:22 +0700

    asd
    asd

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname     = "mysql.hostinger.vn";
+$username     = "root";
+$password     = "";
+$databasename = "banhang_database";
+$visitorTimeout = 900; // =15 * 60 Download source mien phi tai Sharecode.vn
+
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+
+);
+
+?>
+
